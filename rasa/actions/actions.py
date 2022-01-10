09ea1f68d84b920601a25954b5948976021b1232
danielyudicarvalho <danielyudicarvalho@gmail.com>
2022-01-10 18:38:55 -0400

    actions

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -19,7 +19,14 @@ class ActionAnswerDisease(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         disease_slot = tracker.get_slot("disease")
-        utter_response = 'utter_answer_{disease}'.format(disease=disease_slot)
-        dispatcher.utter_message(response=utter_response)
+        utter_response_prevent = 'utter_answer_initial_info_{disease}'.format(disease=disease_slot)
+        utter_response_prevent = 'utter_answer_prevent_{disease}'.format(disease=disease_slot)
+        utter_response_symptoms = 'utter_answer_symptoms_{disease}'.format(disease=disease_slot)
+        utter_response_treatment = 'utter_answer_treatment_{disease}'.format(disease=disease_slot)
+        utter_response_more_info = 'utter_answer_more_info_{disease}'.format(disease=disease_slot)
+        dispatcher.utter_message(response=utter_response_prevent)
+        dispatcher.utter_message(response=utter_response_symptoms)
+        dispatcher.utter_message(response=utter_response_treatment)
+        dispatcher.utter_message(response=utter_response_more_info)
 
         return []
