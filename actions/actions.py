4c2c387679af522b75b1a0e9fb3aa756d77d2bb9
danielyudicarvalho <danielyudicarvalho@gmail.com>
2022-01-12 16:30:43 -0400

    FAQ-diseases

--- actions/actions.py
+++ actions/actions.py
@@ -5,28 +5,85 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-DISEASE = {
-    'leishmaniose'
-}
+QUESTION = {
+     "prevenção": "prevent",
+     "tratamentos":"treatments",
+     "sintomas":"symptoms" ,
+     "sintoma":"symptoms",
+     "prevenir":"prevent",
+     "tratamento":"treatments"
+ }
+
+DISEASE=['leishmaniose','raiva','sarna','toxoplasmose']
 
 class ActionAnswerDisease(Action):
 
     def name(self) -> Text:
-        return "action_hello_world"
+        return "action_answer_disease"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         disease_slot = tracker.get_slot("disease")
-        utter_response_prevent = 'utter_answer_initial_info_{disease}'.format(disease=disease_slot)
-        utter_response_prevent = 'utter_answer_prevent_{disease}'.format(disease=disease_slot)
+        question_slot = tracker.get_slot("question")
+
+        if QUESTION[question_slot]:
+            utter_response_answer = 'utter_answer_{question}_{disease}'.format(disease=disease_slot,question=QUESTION[question_slot])
+        else:
+            utter_response_answer = 'utter_answer_initial_info_{disease}'.format(disease=disease_slot)
+        
+        print(utter_response_answer)
+        print(QUESTION[question_slot])
+        dispatcher.utter_message(response=utter_response_answer)
+ 
+
+        return []
+
+class ActionAnswerDiseaseSymptoms(Action):
+
+    def name(self) -> Text:
+        return "action_answer_disease_symptoms"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        disease_slot = tracker.get_slot("disease")
         utter_response_symptoms = 'utter_answer_symptoms_{disease}'.format(disease=disease_slot)
-        utter_response_treatment = 'utter_answer_treatment_{disease}'.format(disease=disease_slot)
-        utter_response_more_info = 'utter_answer_more_info_{disease}'.format(disease=disease_slot)
-        dispatcher.utter_message(response=utter_response_prevent)
         dispatcher.utter_message(response=utter_response_symptoms)
+ 
+
+        return []
+class ActionAnswerDiseasePrevent(Action):
+
+    def name(self) -> Text:
+        return "action_answer_disease_prevent"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        disease_slot = tracker.get_slot("disease")
+        
+        utter_response_prevent = 'utter_answer_prevent_{disease}'.format(disease=disease_slot)
+        dispatcher.utter_message(response=utter_response_prevent)
+
+ 
+
+        return []
+
+class ActionAnswerDiseaseTreatment(Action):
+
+    def name(self) -> Text:
+        return "action_answer_disease_treatment"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        disease_slot = tracker.get_slot("disease")   
+        utter_response_treatment = 'utter_answer_treatment_{disease}'.format(disease=disease_slot)
         dispatcher.utter_message(response=utter_response_treatment)
-        dispatcher.utter_message(response=utter_response_more_info)
 
         return []
