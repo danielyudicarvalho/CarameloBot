9db7142acd69c956803d45b8fa63b5a65d296b90
danielyudicarvalho <danielyudicarvalho@gmail.com>
2022-01-21 06:58:49 -0400

    @danielyudicarvalho (#27)
    * nova-branch
    
    * jupyter notebook/web scrapping
    
    * up
    
    * update-FAQ
    
    * update-ACTIONS
    
    * fix-diseasesActions

--- actions/actions.py
+++ actions/actions.py
@@ -1,16 +1,16 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
 
 from typing import Any, Text, Dict, List
+from urllib import response
+from numpy import extract
+from pydantic import UrlSchemeError
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import AllSlotsReset
+import bs4
+import urllib.request as urllib_request
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 
 QUESTION = {
@@ -23,7 +23,102 @@ QUESTION = {
      "tratar":"treatments"
  }
 
-DISEASE=['leishmaniose','raiva','sarna','toxoplasmose']
+
+#AGE = {
+#      "ageOne": "Abaixo-de-2-meses",
+#      "ageTwo":"2-a-6-meses",
+#      "ageThree":"7-a-11-meses" ,
+#      "ageFour":"1-ano",
+#      "ageFive":"2-anos",
+#      "ageSix":"3-anos",
+#      "ageSeven":"4-anos",
+#      "ageEight":"5-anos",
+#      "ageNine":"6-anos-Acima",
+#  }
+
+AGE3 = ('Abaixo-de-2-meses','2-a-6-meses','7-a-11-meses','1-ano','2-anos','3-anos','4-anos','5-anos','6-anos-Acima')
+
+# async def extractOne(url):
+#     response = urlopen(url)
+#     html = response.read()
+#     soup = BeautifulSoup(html, 'html.parser')
+#     res = soup.findAll('div', class_="listaAnimais")
+#     return res
+
+async def extractTwo(url):
+    responseLink = urlopen(url)
+    htmlLink = responseLink.read()
+    soupLink = BeautifulSoup(htmlLink, 'html.parser')
+    return soupLink
+
+class ActionScrapping(Action):
+
+    def name(self) -> Text:
+        return "action_scrapping"
+
+    async def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]: 
+
+        size_slot = tracker.get_slot("size")
+        age_slot = tracker.get_slot("age")
+        animal_type_slot = tracker.get_slot("animal_type")
+        gender_slot = tracker.get_slot("gender")
+        urls = []
+        
+        if age_slot == 'baby':
+            urls = ['https://adotar.com.br/animais.aspx?cc=1484&cn=ms-campo-grande&finalidade=Adocao&tipo={tipo}&porte={porte}&idade={idade}&sexo={sexo}'.format(tipo=animal_type_slot,idade=AGE3[0],porte=size_slot,sexo=gender_slot),
+                    'https://adotar.com.br/animais.aspx?cc=1484&cn=ms-campo-grande&finalidade=Adocao&tipo={tipo}&porte={porte}&idade={idade}&sexo={sexo}'.format(tipo=animal_type_slot,idade=AGE3[1],porte=size_slot,sexo=gender_slot)]
+        elif age_slot == 'children':
+            urls = ['https://adotar.com.br/animais.aspx?cc=1484&cn=ms-campo-grande&finalidade=Adocao&tipo={tipo}&porte={porte}&idade={idade}&sexo={sexo}'.format(tipo=animal_type_slot,idade=AGE3[2],porte=size_slot,sexo=gender_slot),
+                    'https://adotar.com.br/animais.aspx?cc=1484&cn=ms-campo-grande&finalidade=Adocao&tipo={tipo}&porte={porte}&idade={idade}&sexo={sexo}'.format(tipo=animal_type_slot,idade=AGE3[3],porte=size_slot,sexo=gender_slot)]
+        else:
+            for i in AGE3[4:]:
+                urls.append('https://adotar.com.br/animais.aspx?cc=1484&cn=ms-campo-grande&finalidade=Adocao&tipo={tipo}&porte={porte}&idade={idade}&sexo={sexo}'.format(tipo=animal_type_slot,idade=i,porte=size_slot,sexo=gender_slot))
+
+        #url_response = 'https://adotar.com.br/animais.aspx?cc=1484&cn=ms-campo-grande&finalidade=Adocao&tipo={tipo}&porte={porte}&idade={idade}&sexo={sexo}'.format(tipo=animal_type_slot,idade=AGE2[1],porte=size_slot,sexo=gender_slot)
+        print(urls)
+
+        for i in urls:
+            response = urlopen(i)
+            html = response.read()
+            soup = BeautifulSoup(html, 'html.parser')
+            res = soup.findAll('div', class_="listaAnimais")
+            print(len(res))
+            if len(res)>0:
+                for item in res:
+                    link = item.find('a')['href']
+                    link = 'https://adotar.com.br'+link
+                    print(link)
+                    dispatcher.utter_message(text=link)
+                    photo = 'https://'+item.find('img')['src'][2:]
+                    print(photo)
+                    dispatcher.utter_message(image=photo)
+                    name = item.find('div',{'class':'listaAnimaisDados'})
+                    name = name.get_text().split()
+                    print(name[0])
+                    dispatcher.utter_message(text=name[0])
+
+            # responseLink = urlopen(link)
+            # htmlLink = responseLink.read()
+            # soupLink = BeautifulSoup(htmlLink, 'html.parser')
+
+            #soupLink = await extractTwo(link)
+            #contact = soupLink.find('a',{"id":"mailprop"})
+            # email = contact['href']
+            # phone = contact.findNextSibling().find('a').getText()
+            # print(str(email[7:]))
+            # print(str(phone))
+            # dispatcher.utter_message(text='email: {mail}'.format(mail=email[6:]))
+            # dispatcher.utter_message(text='telefone: {telefone}'.format(telefone=phone))
+        
+            
+
+        
+      
+        urls =[]
+        return [AllSlotsReset()]
+
 
 #================================================================== 
 # ActionAnswerDisease - implementa uma função para falar  
@@ -50,7 +145,7 @@ class ActionAnswerDisease(Action):
         dispatcher.utter_message(response=utter_response_answer)
  
 
-        return []
+        return [AllSlotsReset()]
 #================================================================== 
 # ActionAnswerDiseaseSymptoms - implementa uma função para falar  
 # sobre os sintomas das zoonoses
@@ -129,4 +224,3 @@ class ActionAboutVaccine(Action):
         dispatcher.utter_message(text="Entrei na vacina!")
 
         return []
-
