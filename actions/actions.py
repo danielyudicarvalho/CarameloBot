426d9d52cdfb76e91056dc9f106b2d459fb79ebb
danielyudicarvalho <danielyudicarvalho@gmail.com>
2022-01-21 15:01:28 -0400

    remove-actions-methods

--- actions/actions.py
+++ actions/actions.py
@@ -27,33 +27,6 @@ QUESTION = {
  }
 
 
-#AGE = {
-#      "ageOne": "Abaixo-de-2-meses",
-#      "ageTwo":"2-a-6-meses",
-#      "ageThree":"7-a-11-meses" ,
-#      "ageFour":"1-ano",
-#      "ageFive":"2-anos",
-#      "ageSix":"3-anos",
-#      "ageSeven":"4-anos",
-#      "ageEight":"5-anos",
-#      "ageNine":"6-anos-Acima",
-#  }
-
-AGE3 = ('Abaixo-de-2-meses','2-a-6-meses','7-a-11-meses','1-ano','2-anos','3-anos','4-anos','5-anos','6-anos-Acima')
-
-# async def extractOne(url):
-#     response = urlopen(url)
-#     html = response.read()
-#     soup = BeautifulSoup(html, 'html.parser')
-#     res = soup.findAll('div', class_="listaAnimais")
-#     return res
-
-async def extractTwo(url):
-    responseLink = urlopen(url)
-    htmlLink = responseLink.read()
-    soupLink = BeautifulSoup(htmlLink, 'html.parser')
-    return soupLink
-
 class ActionScrapping(Action):
 
     def name(self) -> Text:
@@ -72,61 +45,32 @@ class ActionScrapping(Action):
         cluster = MongoClient("mongodb+srv://danielyudi:elysium4@cluster0.catne.mongodb.net/mydatabase?retryWrites=true&w=majority")
         db = cluster["mydatabase"]
         mycol = db["pets"]
-        pets = mycol.find({"size":size_slot,"age":age_slot,"animal_type":animal_type_slot,"gender":gender_slot})
+        pets = list(mycol.find({"goal":"Adocao","size":size_slot,"age":age_slot,"animal_type":animal_type_slot,"gender":gender_slot}))
+     
+        if len(pets)<3:
+            index=0
+            for pet in pets:
+                index+=1
+                dispatcher.utter_message(text=pet['link'])
+                dispatcher.utter_message(text=pet['name'])
+                dispatcher.utter_message(image=pet['photo'])
+                dispatcher.utter_message(text=pet['phone'])
+                dispatcher.utter_message(text=pet['email'])
+            
+        elif len(pets)>5:
+            for i in range(0,2):
+                dispatcher.utter_message(text=pets[i]['link'])
+                dispatcher.utter_message(text=pets[i]['name'])
+                dispatcher.utter_message(image=pets[i]['photo'])
+                dispatcher.utter_message(text=pets[i]['phone'])
+                dispatcher.utter_message(text=pets[i]['email'])
+            
+            dispatcher.utter_message(text="Acesse o site caso não tenha encontrado o que estava buscando")
+            dispatcher.utter_message(text="https://adotar.com.br/busca.aspx?cc=1484&cn=ms-campo-grande")
 
-        for pet in pets:
-            dispatcher.utter_message(text=pet['link'])
-            dispatcher.utter_message(text=pet['name'])
-            dispatcher.utter_message(image=pet['photo'])
-            dispatcher.utter_message(text=pet['phone'])
-            dispatcher.utter_message(text=pet['email'])
-
-        # if age_slot == 'baby':
-        #     urls = ['https://adotar.com.br/animais.aspx?cc=1484&cn=ms-campo-grande&finalidade=Adocao&tipo={tipo}&porte={porte}&idade={idade}&sexo={sexo}'.format(tipo=animal_type_slot,idade=AGE3[0],porte=size_slot,sexo=gender_slot),
-        #             'https://adotar.com.br/animais.aspx?cc=1484&cn=ms-campo-grande&finalidade=Adocao&tipo={tipo}&porte={porte}&idade={idade}&sexo={sexo}'.format(tipo=animal_type_slot,idade=AGE3[1],porte=size_slot,sexo=gender_slot)]
-        # elif age_slot == 'children':
-        #     urls = ['https://adotar.com.br/animais.aspx?cc=1484&cn=ms-campo-grande&finalidade=Adocao&tipo={tipo}&porte={porte}&idade={idade}&sexo={sexo}'.format(tipo=animal_type_slot,idade=AGE3[2],porte=size_slot,sexo=gender_slot),
-        #             'https://adotar.com.br/animais.aspx?cc=1484&cn=ms-campo-grande&finalidade=Adocao&tipo={tipo}&porte={porte}&idade={idade}&sexo={sexo}'.format(tipo=animal_type_slot,idade=AGE3[3],porte=size_slot,sexo=gender_slot)]
-        # else:
-        #     for i in AGE3[4:]:
-        #         urls.append('https://adotar.com.br/animais.aspx?cc=1484&cn=ms-campo-grande&finalidade=Adocao&tipo={tipo}&porte={porte}&idade={idade}&sexo={sexo}'.format(tipo=animal_type_slot,idade=i,porte=size_slot,sexo=gender_slot))
-
-        # #url_response = 'https://adotar.com.br/animais.aspx?cc=1484&cn=ms-campo-grande&finalidade=Adocao&tipo={tipo}&porte={porte}&idade={idade}&sexo={sexo}'.format(tipo=animal_type_slot,idade=AGE2[1],porte=size_slot,sexo=gender_slot)
-        # print(urls)
-
-        # for i in urls:
-        #     response = urlopen(i)
-        #     html = response.read()
-        #     soup = BeautifulSoup(html, 'html.parser')
-        #     res = soup.findAll('div', class_="listaAnimais")
-        #     print(len(res))
-        #     if len(res)>0:
-        #         for item in res:
-        #             link = item.find('a')['href']
-        #             link = 'https://adotar.com.br'+link
-        #             print(link)
-        #             dispatcher.utter_message(text=link)
-        #             photo = 'https://'+item.find('img')['src'][2:]
-        #             print(photo)
-        #             dispatcher.utter_message(image=photo)
-        #             name = item.find('div',{'class':'listaAnimaisDados'})
-        #             name = name.get_text().split()
-        #             print(name[0])
-        #             dispatcher.utter_message(text=name[0])
-
-        #     # responseLink = urlopen(link)
-        #     # htmlLink = responseLink.read()
-        #     # soupLink = BeautifulSoup(htmlLink, 'html.parser')
-
-        #     #soupLink = await extractTwo(link)
-        #     #contact = soupLink.find('a',{"id":"mailprop"})
-        #     # email = contact['href']
-        #     # phone = contact.findNextSibling().find('a').getText()
-        #     # print(str(email[7:]))
-        #     # print(str(phone))
-        #     # dispatcher.utter_message(text='email: {mail}'.format(mail=email[6:]))
-        #     # dispatcher.utter_message(text='telefone: {telefone}'.format(telefone=phone))
-        
+        else:
+            dispatcher.utter_message(text="Infelizmente não encontramos nenhum resultado para sua busca. Você pode fazer uma busca mais aprofundada nesse site:")
+            dispatcher.utter_message(text="https://adotar.com.br/busca.aspx?cc=1484&cn=ms-campo-grande")
             
 
         
@@ -161,81 +105,3 @@ class ActionAnswerDisease(Action):
  
 
         return [AllSlotsReset()]
-#================================================================== 
-# ActionAnswerDiseaseSymptoms - implementa uma função para falar  
-# sobre os sintomas das zoonoses
-#==================================================================
-class ActionAnswerDiseaseSymptoms(Action):
-
-    def name(self) -> Text:
-        return "action_answer_disease_symptoms"
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        disease_slot = tracker.get_slot("disease")
-        utter_response_symptoms = 'utter_answer_symptoms_{disease}'.format(disease=disease_slot)
-        dispatcher.utter_message(response=utter_response_symptoms)
- 
-
-        return []
-#================================================================== 
-# ActionAnswerDiseasePrevent - implementa uma função para falar  
-# sobre a prevenção das zoonoses
-#==================================================================
-
-class ActionAnswerDiseasePrevent(Action):
-
-    def name(self) -> Text:
-        return "action_answer_disease_prevent"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
-        disease_slot = tracker.get_slot("disease")
-        
-        utter_response_prevent = 'utter_answer_prevent_{disease}'.format(disease=disease_slot)
-        dispatcher.utter_message(response=utter_response_prevent)
-
- 
-
-        return []
-
-#================================================================== 
-# ActionAnswerDiseaseTreatment - implementa uma função para falar  
-# sobre o tratamento das zoonoses
-#==================================================================
-      
-class ActionAnswerDiseaseTreatment(Action):
-
-    def name(self) -> Text:
-        return "action_answer_disease_treatment"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        disease_slot = tracker.get_slot("disease")   
-        utter_response_treatment = 'utter_answer_treatment_{disease}'.format(disease=disease_slot)
-        dispatcher.utter_message(response=utter_response_treatment)
-
-        return []
-      
-#================================================================== 
-# Action about vaccine - implementa uma função para falar sobre 
-# as vacinas
-#==================================================================
-      
-class ActionAboutVaccine(Action):
-
-    def name(self) -> Text:
-        return "action_about_vaccine"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        dispatcher.utter_message(text="Entrei na vacina!")
-
-        return []
