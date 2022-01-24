fa080582f24b452a214d944227dfe2e7505e9826
Leo0liveira <leoufms@gmail.com>
2022-01-23 21:18:23 -0300

    [Chore] Adicionando branch com os resultados dos testes

--- telegram_connector.py
+++
@@ -1,7 +0,0 @@
-from rasa.core.channels import TelegramInput
-from typing import Text
-
-class TelegramInputChannel(TelegramInput):
-    def get_metadata(self, request):
-        metadata=request.json
-        return metadata
